155f4f145f7a2d59674780530315ae320a819892
shashank-045 <153505272+shashank-045@users.noreply.github.com>
2024-08-15 21:44:54 +0530

    Update app.js
    cors 2

--- app.js
+++ app.js
@@ -27,11 +27,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+  origin: 'http://localhost:5173', // or any other origin you want to allow
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
